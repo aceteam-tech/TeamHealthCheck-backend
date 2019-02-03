041da7a63ec901927211dab7ef8c2539356b89fe
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-02-03 14:14:55 +0100

    Joining the team

--- functions/functions/team/join-team.js
+++ functions/functions/team/join-team.js
@@ -1,13 +1,21 @@
 const joinTeam = require('../../helpers/join-team.helper');
+const queryTeamsByCodeDB = require('../../database/teams/query-team-by-code.dynamodb')
+const getProfilesDB = require('../../database/profiles/get-batch-profiles.dynamodb')
 
 module.exports.lambda = async (event) => {
     const profileId = event.requestContext.authorizer.claims.sub
-    const {teamId} = JSON.parse(event.body)
+    const {code} = JSON.parse(event.body)
 
-    const team = await joinTeam(teamId, profileId);
+    const teams = await queryTeamsByCodeDB(code)
+    const team = await joinTeam(teams[0].id, profileId)
+    const users = await getProfilesDB(team.users)
+    const teamWithUsers = {
+        ...team,
+        users
+    }
 
     return {
         statusCode: 200,
-        body: JSON.stringify(team)
+        body: JSON.stringify(teamWithUsers)
     }
 };
