dd4d8e3cdc67f05a234dcbe9944a950fc2c53ef9
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-03-30 13:16:10 +0100

    End health check refactor

--- functions/functions/health-check/__test__/end-health-check.test.js
+++ functions/functions/health-check/__test__/end-health-check.test.js
@@ -1,65 +1,103 @@
-const calculateCategories = require('../end-health-check').calculateCategories
+const endHealthCheck = require('../end-health-check')
+const flattenVotes = endHealthCheck.flattenVotes
+const calculateCategories = endHealthCheck.calculateCategories
 
-const voteCategories = [
-    {"id": "1", "value": 1},
-    {"id": "2", "value": 2},
-    {"id": "3", "value": 0}
-]
-const healthCategories = [
-    {"id": "1",},
-    {"id": "2",},
-    {"id": "3",},
-]
+describe('flattenVotes function', () => {
+    it('For 2 full health check statuses', () => {
+        const categories = [
+            { "id": "1", "value": 1 },
+            { "id": "2", "value": 2 }
+        ]
+        const votes = [
+            { categories },
+            { categories }
+        ]
+        const expected = [
+            ...categories,
+            ...categories
+        ]
+        const flattenedVotes = flattenVotes(votes)
+        expect(flattenedVotes).toEqual(expected)
+    })
+})
 
 describe('calculateCategories function', () => {
+    // Function takes two attributes
+    // Votes - An array of votes.
+    let votes = [{
+        categories: []
+    }]
+    // Each vote contains categories property which is an array of health check categories with their values.
+    let voteCategories = [
+        { "id": "1", "value": 1 },
+        { "id": "2", "value": 2 },
+        { "id": "3", "value": 0 }
+    ]
+    // Health check categories - An array of health check categories.
+    const healthCategories = [
+        { "id": "1" },
+        { "id": "2" },
+        { "id": "3" }
+    ]
+
     describe('Happy path', () => {
         it('For 2 full health check statuses', () => {
-            const votes = [
-                {categories: voteCategories},
-                {categories: voteCategories}
+            votes = [
+                ...voteCategories,
+                ...voteCategories
             ]
             const expected = [
-                {"id": "1", "value": 50},
-                {"id": "2", "value": 100},
-                {"id": "3", "value": 0},
+                { "id": "1", "value": 50 },
+                { "id": "2", "value": 100 },
+                { "id": "3", "value": 0 }
             ]
             const calculatedCategories = calculateCategories(votes, healthCategories)
             expect(calculatedCategories).toEqual(expected)
         })
         it('For 2 unfull health check statuses', () => {
-            const category1 = {"id": "1", "value": 1}
-            const votes = [
-                {categories: [category1, {"id": "3", "value": 0}]},
-                {categories: [category1, {"id": "2", "value": 2}]}
+            const category1 = { "id": "1", "value": 1 }
+            votes = [
+                ...[category1, { "id": "3", "value": 0 }],
+                ...[category1, { "id": "2", "value": 2 }]
             ]
             const expected = [
-                {"id": "1", "value": 50},
-                {"id": "2", "value": 100},
-                {"id": "3", "value": 0},
+                { "id": "1", "value": 50 },
+                { "id": "2", "value": 100 },
+                { "id": "3", "value": 0 }
             ]
             const calculatedCategories = calculateCategories(votes, healthCategories)
             expect(calculatedCategories).toEqual(expected)
         })
         it('For 1 full health check status', () => {
-            const votes = [{categories: voteCategories}]
+            votes = voteCategories
             const expected = [
-                {"id": "1", "value": 50},
-                {"id": "2", "value": 100},
-                {"id": "3", "value": 0},
+                { "id": "1", "value": 50 },
+                { "id": "2", "value": 100 },
+                { "id": "3", "value": 0 }
             ]
             const calculatedCategories = calculateCategories(votes, healthCategories)
             expect(calculatedCategories).toEqual(expected)
         })
         it('For 1 not full health check status', () => {
-            const votes = [{
-                categories: [
-                    {"id": "1", "value": 1},
-                    {"id": "3", "value": 0}
-                ]
-            }]
+            votes = [{ "id": "1", "value": 1 },
+                { "id": "3", "value": 0 }]
             const expected = [
-                {"id": "1", "value": 50},
-                {"id": "3", "value": 0},
+                { "id": "1", "value": 50 },
+                { "id": "3", "value": 0 }
+            ]
+            const calculatedCategories = calculateCategories(votes, healthCategories)
+            expect(calculatedCategories).toEqual(expected)
+        })
+        it('For 1 full health check with all zeros', () => {
+            votes = [
+                { "id": "1", "value": 0 },
+                { "id": "2", "value": 0 },
+                { "id": "3", "value": 0 }
+            ]
+            const expected = [
+                { "id": "1", "value": 0 },
+                { "id": "2", "value": 0 },
+                { "id": "3", "value": 0 }
             ]
             const calculatedCategories = calculateCategories(votes, healthCategories)
             expect(calculatedCategories).toEqual(expected)
